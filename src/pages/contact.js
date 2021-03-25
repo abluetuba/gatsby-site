b30dffb2a9d9c5fe298d57dd12c123b5bd1ca49d
abluetuba <57290334+abluetuba@users.noreply.github.com>
2021-03-25 15:48:09 +0100

    add contact form

---
+++ src/pages/contact.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+
+export default function projects({ data }) {
+  return (
+    <Layout>
+      <SEO title="tuba's contact form" />
+      <h1>contact</h1>
+      <form
+        method="post"
+        netlify-honeypot="bot-field"
+        data-netlify="true"
+        name="contact"
+      >
+        <input type="hidden" name="bot-field" />
+        <input type="hidden" name="form-name" value="contact" />
+        <label>
+          Name
+          <input type="text" name="name" id="name" />
+        </label>
+        <label>
+          Email
+          <input type="email" name="email" id="email" />
+        </label>
+        <label>
+          Subject
+          <input type="text" name="subject" id="subject" />
+        </label>
+        <label>
+          Message
+          <textarea name="message" id="message" rows="5" />
+        </label>
+        <button type="submit">Send</button>
+        <input type="reset" value="Clear" />
+      </form>
+    </Layout>
+  );
+}
